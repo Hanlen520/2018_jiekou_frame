f96c0cc4b8c0f023d54adec9105da3594bb7a255
zhangwenkal <zhangwenkal@yeah.net>
2018-05-07 18:34:52 +0800

    新增定制请求头部信息

--- TestCase/get_empinfo_test.py
+++ TestCase/get_empinfo_test.py
@@ -3,43 +3,31 @@ import os
 import requests
 from public.TestApi import testApi
 from public.read_csv import get_csv_data
+from public.get_token import get_token
 from ddt import ddt, data, file_data, unpack
 from conf.config import get_config
 
-
 base_url=get_config()['host']
 
-
-a=get_csv_data('login.csv')
-title=["grant_type","client_id","client_secret","username","password","scope","device_token"]
-
-s = requests.Session()
-r=s.post(base_url+a[0][1],dict(zip(title,a[0][2:-2])))
-# print(r.json()["access_token"])
-# print(r.json())
-
-
-r1=requests.get(base_url+"/api/lebang/staffs/me/detail",params={"token":"Bearer"+' '+r.json()["access_token"]})
-print(r1.request.headers)
-print(r1.json())
-
-
-
-"""
 #获取当前员工信息,需要登录
 class Test_eminfo(unittest.TestCase):
-    def test_eminfo(self):
-        r=requests.get(base_url+"/api/lebang/staffs/me/detail")
+    #用户登录了
+    def test_eminfo1(self):
+        r=requests.get(base_url+"/api/lebang/staffs/me/detail", headers={"Authorization": "Bearer"+' '+get_token()})
         #1.查询成功时候断言
         if r.json()["code"]==0:
-            self.assertEqual(a[-1], r.json()["result"]["mobile"])
-        #查询失败时候断言
+            self.assertEqual("菜头1234", r.json()["result"]["nickname"])
+            self.assertIn("http://7xnc5a.com2",r.json()["result"]["avatar_url"])
+        #请求失败
         else:
-            self.assertEqual(int(a[-2]),r.json()["code"])
-            self.assertEqual(a[-1],r.json()["error"])
-
+            raise Exception("请求错误！")
+    #用户未登录
+    def test_eminfo2(self):
+        r = requests.get(base_url + "/api/lebang/staffs/me/detail")
+        self.assertEqual(123,r.json()['code'])
+        self.assertEqual("请重新登录",r.json()["error"])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-"""
+
 
---
+++ public/get_token.py
@@ -0,0 +1,26 @@
+import requests
+from conf.config import get_config
+from public.read_csv import get_csv_data
+
+a=get_csv_data('login.csv')
+base_url=get_config()['host']
+title=["grant_type","client_id","client_secret","username","password","scope","device_token"]
+
+"""
+s = requests.Session()
+r=s.post(base_url+a[0][1],dict(zip(title,a[0][2:-2])))
+cookies=requests.utils.dict_from_cookiejar(r.cookies)
+print(dict(zip(title,a[0][2:-2])))
+print(r.headers)
+print(r.json())
+print(r.cookies)
+print(r.cookies.get_dict())
+print(r.headers)
+"""
+
+def get_token():
+    r=requests.post(base_url+a[0][1],dict(zip(title,a[0][2:-2])))
+    if r.json()["code"]==0:
+        return r.json()["access_token"]
+    else:
+        raise Exception("请求错误！")
